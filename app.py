7e701927d2f936ea80677ab5ab52dac12e17364b
JackTanRoo <jack.tan.lu@hotmail.com>
2019-03-16 18:29:25 -0700

    moving trader to server side

--- app.py
+++ app.py
@@ -8,4 +8,18 @@
 # print(ingest-exchange -x bitfinex -i btc_usd)
 
 
+# STORIES
+# select crypto to track to track pricing data and make trades
+# select crypto exchange to track pricing data and make trades
+# select fiat pair to track forex rates
+# select forex rate exchange to pull exchange data from
+# pull pricing data from selected crypto exchanges every 1 minute
+# pull forex data from selected forex source every 1 hour
+# plot data as a live graph that updates every 1 minute
+# provide trade suggestions 
+# plot trade suggestions on graph updated every 1 minute
+# plot trade suggestions on the graph every 1 minute
+# make trades to selected trades based on key trade parameters - balance, trading amount, trading currency, trading direction, trading price
+# track balance after every trade
+# calculate profitability of every trade
 
